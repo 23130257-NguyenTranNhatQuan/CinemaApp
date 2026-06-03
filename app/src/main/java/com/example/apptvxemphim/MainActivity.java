b111aeb52d6934b2f397967326c446becf36c4b5
23130257-NguyenTranNhatQuan <23130257@st.hcmuaf.edu.vn>
2026-06-03 21:46:46 +0700

    Initial commit

---
+++ app/src/main/java/com/example/apptvxemphim/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.apptvxemphim;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main); // Gọi file giao diện XML
+
+        // Ẩn thanh Action Bar mặc định đi cho giống app thật
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        // Lắng nghe sự kiện click trên thanh điều hướng
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+
+                // Dùng if-else để tương thích tốt với các phiên bản Gradle mới
+                if (id == R.id.nav_home) {
+                    Toast.makeText(MainActivity.this, "Đang ở Trang chủ", Toast.LENGTH_SHORT).show();
+                    return true;
+                } else if (id == R.id.nav_ticket) {
+                    Toast.makeText(MainActivity.this, "Chuyển sang Mua vé", Toast.LENGTH_SHORT).show();
+                    return true;
+                } else if (id == R.id.nav_news) {
+                    Toast.makeText(MainActivity.this, "Chuyển sang Tin tức", Toast.LENGTH_SHORT).show();
+                    return true;
+                } else if (id == R.id.nav_account) {
+                    Toast.makeText(MainActivity.this, "Chuyển sang Tài khoản", Toast.LENGTH_SHORT).show();
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+}
